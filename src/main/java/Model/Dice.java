8db28ea4a0e996b82397bafecc6064f2d9c0f6e5
Trork <72087050+Trork@users.noreply.github.com>
2020-10-29 14:35:03 +0100

    Oprettet Dice-class, (facevalue, roll og set&get)

--- src/main/java/Model/Dice.java
+++ src/main/java/Model/Dice.java
@@ -1,4 +1,16 @@
 package Model;
 
 public class Dice {
+    private final int MAX = 6;
+    private int faceValue;
+
+    public Dice(int value) { faceValue = value; }
+    public int roll() {
+        faceValue = (int)(Math.random() * MAX) + 1;
+        return faceValue;
+    }
+public void setFaceValue(int value) { faceValue = value; }
+
+    public int getFaceValue() { return faceValue; }
 }
+
