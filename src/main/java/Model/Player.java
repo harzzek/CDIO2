e0a35e81b620da68eedc6a41aecc54bfc11244db
harzzek <39692510+harzzek@users.noreply.github.com>
2020-10-29 14:07:28 +0100

    Har lavet Player klassen

--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -1,4 +1,27 @@
 package Model;
 
 public class Player {
+    String name;
+    int id;
+    int balance;
+
+    //constructor
+    public Player(int id, String name){
+        this.name = name;
+        this.id = id;
+        this.balance = 1000;
+    }
+
+    //Method for updating players score
+    public void updateScore(int balanceUpdate){
+        balance += balanceUpdate;
+    }
+
+    public int getScore(){
+        return balance;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
