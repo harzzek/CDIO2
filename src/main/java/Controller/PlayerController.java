5b641a96a886647b0c8b3d5cb3d3d04bb8d33c25
harzzek <39692510+harzzek@users.noreply.github.com>
2020-10-29 14:21:26 +0100

    Har lavet Player klassen

--- src/main/java/Controller/PlayerController.java
+++ src/main/java/Controller/PlayerController.java
@@ -1,4 +1,38 @@
 package Controller;
 
+import Model.Player;
+
 public class PlayerController {
+
+    private Player[] players = new Player[2];
+
+    public Player createPlayer(int id, String name)
+    {
+        Player player = new Player(id,name);
+        return player;
+    }
+
+    public Player readPlayer(String name)
+    {
+        Player player = null;
+        for (int i = 0; i < players.length; i++)
+        {
+            if(players[i].getName() == name)
+            {
+                player = players[i];
+            }
+        }
+        return player;
+    }
+
+    public void updateScore(String name, int newScore)
+    {
+        for (int i = 0; i < players.length; i++)
+        {
+            if(players[i].getName() == name)
+            {
+                players[i].updateScore(newScore);
+            }
+        }
+    }
 }
