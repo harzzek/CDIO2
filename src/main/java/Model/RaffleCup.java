24caff7e862ab4668ae4c09da2937c5f5cac69ef
Trork <72087050+Trork@users.noreply.github.com>
2020-10-29 15:10:13 +0100

    Opretttet RaffleCup-class (roll, dice1&2, rollArray, set&get, return)

--- src/main/java/Model/RaffleCup.java
+++ src/main/java/Model/RaffleCup.java
@@ -1,4 +1,25 @@
 package Model;
 
 public class RaffleCup {
+        private Dice dice1;
+        private Dice dice2;
+        private int value;
+
+
+
+public RaffleCup() {
+        Dice dice1 = new Dice();
+        Dice dice2 = new Dice();
+        this.dice1 = dice1;
+        this.dice2 = dice2;
+ }
+
+ public int[] roll() {
+        int[] rollArray = new int[2];
+        dice1.setFaceValue(dice1.roll());
+        dice2.setFaceValue(dice2.roll());
+        rollArray[0] = dice1.getFaceValue();
+        rollArray[1] = dice2.getFaceValue();
+        return rollArray;
+ }
 }
