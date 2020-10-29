769329e498ac6a854ba965964697d7bcb0dd60d1
Trork <72087050+Trork@users.noreply.github.com>
2020-10-29 19:06:49 +0100

    Tilføjet Square.java: name, description Tilføjet GameBoard.java: squares[], new Square, id, points, name, description

--- src/main/java/Model/GameBoard.java
+++ src/main/java/Model/GameBoard.java
@@ -8,14 +8,14 @@ public class GameBoard
 {
 
     GUI_Field[] fields = new GUI_Field[12];
-
+    Square[] squares = new Square[12];
     public GameBoard()
     {
     }
 
     public GUI_Field[] createFields()
     {
-        fields[0] = new GUI_Start("Start","This is were you start stuped","Well what can i say?", Color.RED,Color.black);
+        fields[0] = new GUI_Start("Start","This is were you start stupid","Well what can i say?", Color.RED,Color.black);
         for (int i = 1; i < fields.length; i++)
         {
             fields[i] = new GUI_Street();
@@ -27,11 +27,20 @@ public class GameBoard
         return fields;
     }
 
-    public Square createSquare()
+    public void createSquare()
     {
-        int x;
-        int y;
-        Square square = new Square(GUI_Street,x,y);
+        squares[0] = new Square(1, 0, "Endless Plains", "You've entered the Endless Plains, nothing of interest here");
+        squares[1] = new Square(2, +250, "Tower", "You enter the tower and find 250 points" );
+        squares[2] = new Square(3, -100, "Crater","What's in here? You explore the crater and lost 100 points");
+        squares[3] = new Square(4, +100, "Palace Gates","Hurray! You're welcomed back at the gates and receive 100 points");
+        squares[4] = new Square(5, -20, "Cold Desert","Brrr! You're freezing in the Cold Desert and paid 20 points for a hide");
+        squares[5] = new Square(6, +180, "Walled City","You enter the Walled City and find 180 points");
+        squares[6] = new Square(7, 0, "Monastery","You spend the night at the Monastery for free");
+        squares[7] = new Square(8, -70, "Black Cave","You've entered the Black Cave and can't find you're way out, pay 70 points for a way out");
+        squares[8] = new Square(9, +60, "Huts in the Mountain","You're in the mountains and stumble upon some huts, you find 60 points");
+        squares[9] = new Square(10, -80, "The Werewall", "AHH! You've encountered the were-wolfs and they stole 80 points from you, but you get an extra turn");
+        squares[10] = new Square(11, -50, "The Pit","Ouch! You fell into the Pit and lost 50 points");
+        squares[11] = new Square(12, +650, "Goldmine","Money! You're work at the Goldmine is appreciated, rewards 650 points");
 
     }
 
--- src/main/java/Model/Square.java
+++ src/main/java/Model/Square.java
@@ -1,18 +1,24 @@
 package Model;
 
 import gui_fields.GUI_Field;
+import gui_fields.GUI_Street;
 
 public class Square {
 
     private int id;
     private GUI_Field type;
     private int points;
+    private String name;
+    private String description;
 
-    public Square(GUI_Field type,int id,int points)
+
+    public Square(int id,int points, String name, String description)
     {
         this.id = id;
-        this.type = type;
+        type = new GUI_Street();
         this.points = points;
+        this.name = name;
+        this.description = description;
     }
 
     public String getId() {
